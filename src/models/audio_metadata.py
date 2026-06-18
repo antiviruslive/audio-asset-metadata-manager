7f844d92330dcf615c729528314b5e53f2940482
Norton Henrique Silva <antivirus.live00@gmail.com>
2026-06-18 03:25:30 -0300

    Create metadata model and audio reader

---
+++ src/models/audio_metadata.py
@@ -0,0 +1,41 @@
+class AudioMetadata:
+
+    def __init__(
+        self,
+        filename,
+        title,
+        artist,
+        album,
+        genre,
+        year,
+        track_number,
+        duration,
+        sample_rate,
+        channels
+    ):
+
+        self.filename = filename
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.genre = genre
+        self.year = year
+        self.track_number = track_number
+        self.duration = duration
+        self.sample_rate = sample_rate
+        self.channels = channels
+
+    def to_dict(self):
+
+        return {
+            "filename": self.filename,
+            "title": self.title,
+            "artist": self.artist,
+            "album": self.album,
+            "genre": self.genre,
+            "year": self.year,
+            "track_number": self.track_number,
+            "duration": self.duration,
+            "sample_rate": self.sample_rate,
+            "channels": self.channels
+        }
